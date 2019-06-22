3362252efc9d4d4a4dc39bf4cb396886680fdc28
Matheus Oliveira <matheus.oliveira2@estudante.ufla.br>
2019-06-22 13:32:57 -0300

    máquina pronta

---
+++ uh.py
@@ -0,0 +1,198 @@
+import sys
+import re
+import os
+import time
+
+RED   = "\033[1;31m"
+RESET = "\033[0;0m"
+
+class Transicao:
+    def __init__(self,estadoDestino,troca,movimento,estadoAtual,leitura):
+        self.estadoDestino = estadoDestino
+        self.troca = troca
+        self.movimento = movimento
+        self.estadoAtual = estadoAtual
+        self.leitura = leitura
+
+def simbolo(valor):
+    dic = {1: 'a', 2:'b',3:'B'}
+    return(dic[valor])
+
+def direcao(valor):
+    dic = {1: 'R', 2:'L'}
+    return(dic[valor])
+
+def decodificaEntrada(entrada):
+    maquina = []
+    palavra = []
+    flagFimMaquina = False
+    countZero = 0
+    for i in range(len(entrada)):
+        if(not flagFimMaquina):
+            if(entrada[i] != "\n"):
+                maquina.append(entrada[i])
+                if(entrada[i] == '0'):  
+                    countZero = countZero + 1
+                else:
+                    countZero = 3
+                if(countZero == 6):
+                    flagFimMaquina = True
+        else:
+            if(entrada[i] != "\n"):
+                palavra.append(entrada[i])
+        
+    return maquina,palavra
+
+def lerArquivo():
+    arquivo = sys.argv[1]
+    f = open(arquivo,'r')
+    data = f.read()
+    f.close()
+    return data
+
+def verificaMTU(texto):
+    expressao="(000)(((1+)(0)(1+)(0)(1+)(0)(1+)(0)(1+)00))*((1+)(0)(1+)(0)(1+)(0)(1+)(0)(1+))(000)(1+)(01+)*(000)"
+    x = bool(re.match(expressao,str(texto)))
+    if(x):
+        return True
+    else:
+        print("False")
+        return False
+
+def decodificaFita(palavra):
+    fitaEntrada = []
+    count = 0
+    for i in range(len(palavra)):
+        if(palavra[i] == "0"):
+            count = 0
+        else:
+            count = count + 1
+            if(i<(len(palavra)-1)):
+                if(palavra[i+1] == "0"):
+                    fitaEntrada.append(simbolo(count))
+                    count = 0
+    
+    fitaEntrada.append("B")
+    
+    #print(fitaEntrada)
+    return fitaEntrada
+
+def moveRight(posicao, maquina):
+    while(maquina[posicao]!="1" and posicao < len(maquina)-1):
+        posicao = posicao + 1
+    return posicao
+
+def decodificaMaquina(maquina):
+    transicoes = []
+    estado = []
+    posicao = 3
+    acabou = False
+    estadoAtualAux = 0
+    while(posicao < len(maquina)-1):
+        estadoDestino = -1
+        troca = 0
+        movimento = 0
+        estadoAtual = -1
+        leitura = 0
+        while(maquina[posicao] != "0"):
+            estadoAtual = estadoAtual +1
+            posicao = posicao +1
+        posicao = posicao +1
+        while(maquina[posicao] != "0"):
+            leitura = leitura + 1
+            posicao = posicao +1
+        posicao = posicao +1
+        while(maquina[posicao] != "0"):
+            estadoDestino = estadoDestino + 1
+            posicao = posicao +1
+        posicao = posicao +1
+        while(maquina[posicao] != "0"):
+            troca = troca + 1
+            posicao = posicao +1
+        posicao = posicao +1
+        while(maquina[posicao] != "0" and posicao < len(maquina)-1):
+            movimento = movimento + 1
+            posicao = posicao +1
+        
+        #print(estadoAtual, " ", simbolo(leitura), " ", estadoDestino, " ", simbolo(troca), " ", direcao(movimento)) 
+        oEstado = Transicao(estadoDestino,troca,movimento,estadoAtual,leitura)        
+        
+        if(estadoAtualAux == estadoAtual):
+            estado.append(oEstado)
+        else:
+            transicoes.append(estado)
+            estado = []
+            estado.append(oEstado)
+            estadoAtualAux = oEstado.estadoAtual
+        posicao = moveRight(posicao,maquina)
+    return transicoes
+
+
+def executar(transicoes,fita):
+    os.system("clear")
+    estadoAtual = 0
+    terminou = False
+    posicaoFita = 0
+    while(not terminou):    
+        achouTransicao = False
+        for i in range(len(transicoes[estadoAtual])):
+            if(simbolo(transicoes[estadoAtual][i].leitura) == fita[posicaoFita]):
+                
+                print("Estado Atual:", estadoAtual)
+                print("Conteudo da Fita:", fita[posicaoFita])
+                print("Direcao Cabeça de Leitura:",direcao(transicoes[estadoAtual][i].movimento))
+                print("Troca por: ", simbolo(transicoes[estadoAtual][i].troca))
+                fita[posicaoFita] = simbolo(transicoes[estadoAtual][i].troca)
+                posicaoFitaAux = posicaoFita
+                if(direcao(transicoes[estadoAtual][i].movimento) == "R"):
+                    posicaoFita = posicaoFita+1
+                elif(direcao(transicoes[estadoAtual][i].movimento) == "L"):
+                    posicaoFita = posicaoFita-1
+                
+                estadoAtual = transicoes[estadoAtual][i].estadoDestino
+                if(estadoAtual <= len(transicoes)): 
+                    achouTransicao = True
+                for j in range(len(fita)):
+                    if(j==posicaoFitaAux):
+                        print(RED,end="")
+                        print(fita[j],end="")
+                    else:
+                        print(RESET,end="")
+                        print(fita[j],end="")
+                print(RESET)
+                time.sleep(1)
+                #os.system("clear") 
+                break
+
+        if(not achouTransicao):
+            terminou = True
+    print("Estado Atual:", estadoAtual)
+    print("Conteudo da Fita:", fita[posicaoFita])
+    print("Direcao Cabeça de Leitura: NULL")
+    for j in range(len(fita)):
+        if(j==posicaoFita):
+                    print(RED,end="")
+                    print(fita[j],end="")
+        else:
+                    print(RESET,end="")
+                    print(fita[j],end="")
+    print()
+                
+
+        
+         
+            
+        
+       
+
+def main():
+    arquivo = lerArquivo()
+    if(verificaMTU(arquivo) == True ):
+        maquina,palavra = decodificaEntrada(arquivo)
+        fita = decodificaFita(palavra)
+        transicoes = decodificaMaquina(maquina)
+        executar(transicoes,fita)
+    
+    else:
+        print("Entrada inválida")
+main()
